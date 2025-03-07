787acfe5934bc21f6526fd93b66b1db33ef3be94
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-03-07 15:27:37 +0100

    Sjekk at bruker har tilgang til virksomhet og overordnet enhet for å gi tilgang til statistikk Legg til logg

--- src/main/kotlin/no/nav/pia/sykefravarsstatistikk/NaisEnvironment.kt
+++ src/main/kotlin/no/nav/pia/sykefravarsstatistikk/NaisEnvironment.kt
@@ -23,11 +23,7 @@ internal object Systemmiljø {
     val tokenxJwkPath: String by lazy { System.getenv("TOKEN_X_JWKS_URI") }
     val tokenxPrivateJwk: String by lazy { System.getenv("TOKEN_X_PRIVATE_JWK") }
     val tokenXTokenEndpoint: String by lazy { System.getenv("TOKEN_X_TOKEN_ENDPOINT") }
-    val altinnRettigheterProxyUrl: String by lazy { System.getenv("ALTINN_RETTIGHETER_PROXY_URL") }
-    val altinnRettigheterProxyClientId: String by lazy { System.getenv("ALTINN_RETTIGHETER_PROXY_CLIENT_ID") }
     val altinnTilgangerProxyUrl: String by lazy { System.getenv("ALTINN_TILGANGER_PROXY_URL") }
-    val serviceCodeSykefraværsstatistikk: String by lazy { System.getenv("ALTINN_RETTIGHET_SERVICE_CODE") }
-    val serviceEditionSykefraværsstatistikk: String by lazy { System.getenv("ALTINN_RETTIGHET_SERVICE_EDITION") }
     val brregUrl: String by lazy { System.getenv("BRREG_URL") }
     val cluster: String by lazy { System.getenv("NAIS_CLUSTER_NAME") }
 }
