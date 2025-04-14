16de3a1f210f2446b6b075121bc4443f66d2aa28
Sindre Larsen <silar.dev@proton.me>
2025-04-14 10:51:09 +0200

    Kobler sammen nye metrics og endepunkt

--- src/main/kotlin/no/nav/pia/sykefravarsstatistikk/konfigurasjon/plugins/Monitoring.kt
+++ src/main/kotlin/no/nav/pia/sykefravarsstatistikk/konfigurasjon/plugins/Monitoring.kt
@@ -6,18 +6,15 @@ import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
-import io.micrometer.prometheusmetrics.PrometheusConfig
-import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import no.nav.pia.sykefravarsstatistikk.Metrics
 
 fun Application.configureMonitoring() {
-    val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
-
     install(MicrometerMetrics) {
-        registry = appMicrometerRegistry
+        registry = Metrics.appMicrometerRegistry
     }
     routing {
         get("/metrics") {
-            call.respond(appMicrometerRegistry.scrape())
+            call.respond(Metrics.appMicrometerRegistry.scrape())
         }
     }
 }
